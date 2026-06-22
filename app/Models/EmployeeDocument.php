de4cb89cc0c5c5afe5892401349bc09c4683d0ac
Hung Khuat <hungkun69yg@gmail.com>
2026-06-22 08:31:37 +0700

    xem chi tiết quản lí nhân viên

--- app/Models/EmployeeDocument.php
+++ app/Models/EmployeeDocument.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmployeeDocument extends Model
 {
-    //
+    protected $table = 'employee_documents';
+
+    protected $fillable = [
+        'employee_id',
+        'document_name',
+        'document_type',
+        'file_path',
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
--- app/Models/KPI.php
+++ app/Models/KPI.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class KPI extends Model
 {
-    //
+    protected $table = 'kpis';
 }
