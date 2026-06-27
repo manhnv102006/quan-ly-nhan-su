6286dcc559b8d6f0bfba6f159dc712872a23752d
Hung Khuat <hungkun69yg@gmail.com>
2026-06-27 23:21:08 +0700

    xem chi tiết lương của nhân viên

--- resources/views/employee/payrolls/index.blade.php
+++ resources/views/employee/payrolls/index.blade.php
@@ -97,9 +97,14 @@
                                     </span>
                                 </td>
                                 <td class="px-6 py-4 text-right">
-                                    <a href="{{ route('employee.payrolls.pdf', $payroll) }}" class="inline-flex items-center rounded-xl bg-sky-600 px-3 py-2 text-xs font-semibold text-white hover:bg-sky-700">
-                                        Tải PDF
-                                    </a>
+                                    <div class="flex flex-wrap justify-end gap-2">
+                                        <a href="{{ route('employee.payrolls.show', $payroll) }}" class="inline-flex items-center rounded-xl bg-slate-100 px-3 py-2 text-xs font-semibold text-slate-700 hover:bg-slate-200">
+                                            Xem chi tiết
+                                        </a>
+                                        <a href="{{ route('employee.payrolls.pdf', $payroll) }}" class="inline-flex items-center rounded-xl bg-sky-600 px-3 py-2 text-xs font-semibold text-white hover:bg-sky-700">
+                                            Tải PDF
+                                        </a>
+                                    </div>
                                 </td>
                             </tr>
                         @empty
