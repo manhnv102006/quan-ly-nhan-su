9d9d136ecc3b583c7e25c1dd954e659845a90a4a
anhtuan <tuanphunganh28@gmail.com>
2026-06-30 00:01:53 +0700

    fix bug lỗi gây ra thêm thừa bảng

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -15,7 +14,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable;
 
     public function role()
     {
