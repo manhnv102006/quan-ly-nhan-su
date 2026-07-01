bb27f6f410a80acbbc28d29f02d4130751c83286
Thiendez <thienkocony2@gmail.com>
2026-07-02 01:01:16 +0700

    thêm tiêu đề email

--- app/Mail/CandidateInterviewInvitationMail.php
+++ app/Mail/CandidateInterviewInvitationMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use App\Models\Interview;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class CandidateInterviewInvitationMail extends Mailable
@@ -13,5 +14,17 @@ class CandidateInterviewInvitationMail extends Mailable
 
     public function __construct(public Interview $interview)
     {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subjectText()
+        );
+    }
+
+    public function subjectText(): string
+    {
+        return 'Thư mời phỏng vấn tại '.config('app.name');
     }
 }
