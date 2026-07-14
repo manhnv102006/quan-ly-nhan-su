948d700918f9980d083fae7ec8991a5537ff5ccd
hieu27042004 <phieu2704@gmail.com>
2026-07-14 16:37:40 +0700

    feat: thêm module Quản lý Ngày lễ (Holidays) cho Admin

---
+++ resources/views/admin/holidays/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin', ['title' => 'Quản lý Ngày Lễ / Sự kiện'])
+
+@section('content')
+    <div class="mb-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold">Quản lý Ngày Lễ & Sự kiện</h1>
+    </div>
+    <div class="bg-white rounded shadow p-4">
+        <p>Giao diện đang được hoàn thiện...</p>
+    </div>
+@endsection
